38f8408e477c2de3c58decd62eea9d2f7476580f
NikolaPantic <nikolapantic4@gmail.com>
2024-02-22 23:39:23 +0100

    added alt tag to info icons

--- components/InfoCard/InfoCard.jsx
+++ components/InfoCard/InfoCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const InfoCard=({imageSrc, cardHeading, cardText, cardLink, linkText})=>{
 
     return      <div className='info-card'>
-    <Image src={imageSrc} className='info-card__image'/>
+    <Image src={imageSrc} className='info-card__image' alt="Info card icon"/>
     <h2 className='tertiary-heading'>{cardHeading}</h2>
     <p className="info-card__text">{cardText}</p>
     <a href={cardLink} className="text-link">{linkText}</a>
